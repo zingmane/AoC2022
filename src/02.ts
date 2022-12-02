8f120517cdfd8adfebdd4eee0933d54b8d82a24e
Manfred Zingl <mz@campudus.com>
2022-12-02 18:07:45 +0100

    implement second part of day 2

--- src/02.ts
+++ src/02.ts
@@ -8,17 +8,23 @@ const RPS = {
   PAPER: "paper",
   SCISSORS: "scissors",
 } as const;
-
 type RPS = Values<typeof RPS>;
 
-export const run = (raw: string) => {
-  const mapToRPS = (t: string) =>
-    match(t)
-      .with("A", "X", () => RPS.ROCK)
-      .with("B", "Y", () => RPS.PAPER)
-      .with("C", "Z", () => RPS.SCISSORS)
-      .run();
+const MatchResult = {
+  LOSE: "lose",
+  DRAW: "draw",
+  WIN: "win",
+} as const;
+type MatchResult = Values<typeof MatchResult>;
 
+const mapToRPS = (t: string) =>
+  match(t)
+    .with("A", "X", () => RPS.ROCK)
+    .with("B", "Y", () => RPS.PAPER)
+    .with("C", "Z", () => RPS.SCISSORS)
+    .run();
+
+const getPart1 = (raw: string) => {
   const mapToScore = (rps: [RPS, RPS]): number => {
     const winLooseScore = match(rps)
       .with([RPS.ROCK, RPS.ROCK], () => 3)
@@ -55,7 +61,63 @@ export const run = (raw: string) => {
 
   const strategyGuide = splitUp(raw);
 
-  const result = getMatchScore(strategyGuide);
+  return getMatchScore(strategyGuide);
+};
 
-  return [result, 0];
+const getPart2 = (raw: string) => {
+  const mapToMatchResult = (t: string) =>
+    match(t)
+      .with("X", () => MatchResult.LOSE)
+      .with("Y", () => MatchResult.DRAW)
+      .with("Z", () => MatchResult.WIN)
+      .run();
+
+  const mapToScore = (rps: [RPS, MatchResult]): number => {
+    const winLooseScore = match(rps)
+      .with([P._, MatchResult.LOSE], () => 0)
+      .with([P._, MatchResult.DRAW], () => 3)
+      .with([P._, MatchResult.WIN], () => 6)
+      .exhaustive();
+
+    const itemScore = match(rps)
+      .with([RPS.ROCK, MatchResult.LOSE], () => 3) // we have to choose 'scissors'
+      .with([RPS.ROCK, MatchResult.DRAW], () => 1) // we have to choose 'rock'
+      .with([RPS.ROCK, MatchResult.WIN], () => 2) // we have to choose 'paper'
+      .with([RPS.PAPER, MatchResult.LOSE], () => 1) // we have to choose 'rock'
+      .with([RPS.PAPER, MatchResult.DRAW], () => 2) // we have to choose 'paper'
+      .with([RPS.PAPER, MatchResult.WIN], () => 3) // we have to choose 'scissors'
+      .with([RPS.SCISSORS, MatchResult.LOSE], () => 2) // we have to choose 'paper'
+      .with([RPS.SCISSORS, MatchResult.DRAW], () => 3) // we have to choose 'scissors'
+      .with([RPS.SCISSORS, MatchResult.WIN], () => 1) // we have to choose 'rock'
+      .exhaustive();
+
+    return itemScore + winLooseScore;
+  };
+
+  const splitUp = fp.compose(
+    fp.map(splitByBlank),
+    fp.compact,
+    splitByNewline,
+  );
+
+  const mapToInputAndResult = (
+    [enemyInput, matchResult]: [string, string],
+  ) => [mapToRPS(enemyInput), mapToMatchResult(matchResult)];
+
+  const getMatchScore = fp.compose(
+    fp.sum,
+    fp.map(mapToScore),
+    fp.map(mapToInputAndResult),
+  );
+
+  const strategyGuide = splitUp(raw);
+
+  return getMatchScore(strategyGuide);
+};
+
+export const run = (raw: string) => {
+  const part1 = getPart1(raw);
+  const part2 = getPart2(raw);
+
+  return [part1, part2];
 };
